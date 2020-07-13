77a7b3870a8e6178f7ed00fd6ba68d7c5be2147c
Federico <toledofederico11@gmail.com>
2020-07-13 13:33:55 -0300

    added firebase auth & db

--- android/app/src/main/java/com/ecommerce/MainApplication.java
+++ android/app/src/main/java/com/ecommerce/MainApplication.java
@@ -1,10 +1,9 @@
-package com.example;
+package com.ecommerce;
 
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -63,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.example.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.ecommerce.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
